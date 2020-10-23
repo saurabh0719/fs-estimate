1d3e6fbc187ece59fd34d0a07b3bbe68944fe273
Saurabh Pujari <saurabhpuj99@gmail.com>
2020-10-23 21:24:26 +0530

    update

--- bin/Estimator.js
+++ bin/Estimator.js
@@ -9,8 +9,10 @@ class Estimator{
     constructor(){}
 
     extractWords(file_path){
+        console.log("extract words function");
         const data = fs.readFileSync(file_path, {encoding:'utf8', flag: 'r'});
         let words = data.split(' ');
+        console.log(words);
         return words.length;
     }
 
--- bin/index.js
+++ bin/index.js
@@ -3,7 +3,12 @@
 require('yargs')
   .scriptName("pirate-parser")
   .usage('$0 <cmd> [args]')
-  .command('read [filename]', 'Read the file and get reading time', (yargs) => {}, function (argv) {
+  .command('read [filename]', 'Read the file and get reading time', (yargs) => {
+    yargs.positional('filename', {
+        type: 'string',
+        describe: 'The file/file path that you want to read'
+      })
+  }, function (argv) {
    // console.log("We are here")
     const Estimator = require('./Estimator')
     let estimate = new Estimator();
